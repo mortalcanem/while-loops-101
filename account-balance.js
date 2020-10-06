3c32b55e7fb6f96559755382074f392e81de7f55
mortalcanem <66886968+mortalcanem@users.noreply.github.com>
2020-10-05 21:32:14 -0700

    Update account-balance.js
    Module 1 14.6 lab

--- account-balance.js
+++ account-balance.js
@@ -18,3 +18,22 @@ function accountBalance(lines) {
 console.log(`Balance: ${balance.toFixed(2)}`); }
 
 accountBalance(['500', '15.5', '80.35', 'End']);
+
+/// alternate solution using a different while loop. Passes Hw checker. Module 1 14.6 lab
+
+function main(lines) {
+    var balance = 0;
+    var transaction = lines.shift();
+    while (transaction != "End") {
+        var amount = Number(transaction);
+        if (transaction > 0) {
+            console.log(`Increase: ${amount.toFixed(2)}`);
+            balance += amount;
+        } else {
+            console.log(`Decrease: ${Math.abs(amount).toFixed(2)}`);
+            balance -= Math.abs(amount);
+        }
+        transaction = lines.shift();
+    }
+    console.log("Balance: " + balance.toFixed(2));
+}
